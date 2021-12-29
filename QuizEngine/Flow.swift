61bea8997dff632441c01417ec8791bd2aa82ef9
Paulo Silva <paulobfsilva@gmail.com>
2021-12-29 21:57:13 +0000

    Initial Commit

---
+++ QuizEngine/Flow.swift
@@ -0,0 +1,28 @@
+//
+//  Flow.swift
+//  QuizEngine
+//
+//  Created by Paulo Silva on 29/12/2021.
+//
+
+import Foundation
+
+protocol Router {
+    func routeTo(question: String)
+}
+
+class Flow {
+    let router: Router
+    let questions: [String]
+    
+    init(questions: [String], router: Router) {
+        self.questions = questions
+        self.router = router
+    }
+    
+    func start() {
+        if !questions.isEmpty {
+            router.routeTo(question: "")
+        }
+    }
+}
