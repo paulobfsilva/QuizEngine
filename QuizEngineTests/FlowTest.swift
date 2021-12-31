02efc05096785ca7f31cab4ee6fdaecd85f6191a
Paulo Silva <paulobfsilva@gmail.com>
2021-12-31 16:08:45 +0000

    Show result as the accumulated value of questions and answers

--- QuizEngineTests/FlowTest.swift
+++ QuizEngineTests/FlowTest.swift
@@ -69,19 +69,43 @@ class FlowTest: XCTestCase {
         XCTAssertEqual(router.routedQuestions, ["Q1"])
     }
     
+    func test_start_withNoQuestions_routesToResult() {
+        makeSUT(questions: []).start()
+        
+        XCTAssertEqual(router.routedResult, [:])
+    }
+    
+    func test_startAndAnswerFirstAndSecondQuestions_withTwoQuestions_routesToResult() {
+        let sut = makeSUT(questions: ["Q1", "Q2"])
+
+        sut.start()
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+
+        XCTAssertEqual(router.routedResult!, ["Q1": "A1", "Q2": "A2"])
+    }
+    
 //    MARK: - Helpers
     func makeSUT(questions: [String]) -> Flow {
         return Flow(questions: questions, router: router)
     }
     
     class RouterSpy: Router {
+        
         var routedQuestions: [String] = []
+        var routedResult: [String: String]? = nil
+
         var answerCallback: (Router.AnswerCallback) = {_ in }
         
         func routeTo(question: String, answerCallback: @escaping Router.AnswerCallback) {
             routedQuestions.append(question)
             self.answerCallback = answerCallback
         }
+        
+        func routeTo(result: [String : String]) {
+            routedResult = result
+        }
+        
     }
     
 }
